fe09e41a33d656b7625246ad88bd3864e3d9bf64
nrj022 <0602smj@gmail.com>
2023-05-09 23:59:38 +0900

    refactor: 알람, 휴 디렉토리 추가 & bell 이미지 여백 조정

---
+++ app/src/main/java/dodeunifront/dodeuni/alert/AlertActivity.java
@@ -0,0 +1,16 @@
+package dodeunifront.dodeuni.alert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dodeunifront.dodeuni.R;
+
+public class AlertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert);
+    }
+}

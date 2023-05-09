5d107d8cf3c94a986e98cd4c2a7ab094d94063b8
yenidanan <yenidanan@sookmyung.ac.kr>
2023-05-09 20:33:25 +0900

    feat: 하단네비바,상단툴바 구현

---
+++ app/src/main/java/dodeunifront/dodeuni/MainActivity.java
@@ -0,0 +1,65 @@
+package dodeunifront.dodeuni;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    final String TAG = this.getClass().getSimpleName();
+
+    FrameLayout home;
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init(); //객체 정의
+        SettingListener(); //리스너 등록
+
+        //맨 처음 시작할 탭 설정
+        bottomNavigationView.setSelectedItemId(R.id.menu_community);
+    }
+    private void init() {
+        home = findViewById(R.id.content_layout);
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+    }
+    private void SettingListener() {
+        //선택 리스너 등록
+        bottomNavigationView.setOnItemSelectedListener(new TabSelectedListener());
+    }
+
+    class TabSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.menu_community: {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.content_layout, new CommunityFragment())
+                            .commit();
+                    return true;
+                }
+                case R.id.menu_location: {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.content_layout, new CommunityFragment())
+                            .commit();
+                    return true;
+                }
+                case R.id.menu_mypage: {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.content_layout, new CommunityFragment ())
+                            .commit();
+                    return true;
+                }
+
+            }
+
+            return false;
+        }
+    }
+}

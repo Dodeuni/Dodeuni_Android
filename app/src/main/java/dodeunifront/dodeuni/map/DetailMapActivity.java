8c6e2afb1a16e59526a81eb9216d9a1003f42a9b
nrj022 <0602smj@gmail.com>
2023-05-12 21:06:14 +0900

    feat: 맵 장소 리뷰 UI 구현

--- app/src/main/java/dodeunifront/dodeuni/map/DetailMapActivity.java
+++ app/src/main/java/dodeunifront/dodeuni/map/DetailMapActivity.java
@@ -1,6 +1,5 @@
 package dodeunifront.dodeuni.map;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,22 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.google.android.material.bottomsheet.BottomSheetBehavior;
-
-import net.daum.mf.map.api.MapView;
-
-import org.w3c.dom.Text;
 
 import dodeunifront.dodeuni.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link MapFragment#newInstance} factory method to
+ * Use the {@link DetailMapActivity#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MapFragment extends Fragment {
+public class DetailMapActivity extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,11 +25,8 @@ public class MapFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    private View v;
-    private ViewGroup mapViewContainer;
-    private MapView mapView;
 
-    public MapFragment() {
+    public DetailMapActivity() {
         // Required empty public constructor
     }
 
@@ -47,11 +36,11 @@ public class MapFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment MapFragment.
+     * @return A new instance of fragment DetailMapFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static MapFragment newInstance(String param1, String param2) {
-        MapFragment fragment = new MapFragment();
+    public static DetailMapActivity newInstance(String param1, String param2) {
+        DetailMapActivity fragment = new DetailMapActivity();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -71,33 +60,7 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        v = inflater.inflate(R.layout.fragment_map, container, false);
-        mapView = new MapView(getContext());
-        mapViewContainer = (ViewGroup) v.findViewById(R.id.map_view);
-        mapViewContainer.addView(mapView);
-
-        BottomSheetBehavior<View> behavior = BottomSheetBehavior.from(
-                v.findViewById(R.id.drawer_location_recommend)
-        );
-        behavior.setPeekHeight(350);
-
-        TextView tv_new = v.findViewById(R.id.tv_location_new);
-        tv_new.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getContext(), FindLocationMapActivity.class);
-                startActivity(intent);
-                getActivity().finish();
-            }
-        });
-
         // Inflate the layout for this fragment
-        return v;
-    }
-
-    @Override
-    public void onStop() {
-        mapViewContainer.removeView(mapView);
-        super.onStop();
+        return inflater.inflate(R.layout.activity_detail_map, container, false);
     }
 }
---
+++ app/src/main/java/dodeunifront/dodeuni/map/FindLocationMapActivity.java
@@ -0,0 +1,20 @@
+package dodeunifront.dodeuni.map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dodeunifront.dodeuni.R;
+
+public class FindLocationMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_location_map);
+
+        /*MapView mapView = new MapView(this);
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);*/
+    }
+}

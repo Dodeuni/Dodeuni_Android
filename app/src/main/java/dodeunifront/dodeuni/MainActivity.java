835afe1c1f9a59a150acc2f172966381a0c080b6
yenidanan <yenidanan@sookmyung.ac.kr>
2023-05-16 17:36:40 +0900

    feat: 메인액티비티에 툴바 추가

--- app/src/main/java/dodeunifront/dodeuni/MainActivity.java
+++ app/src/main/java/dodeunifront/dodeuni/MainActivity.java
@@ -3,13 +3,20 @@ package dodeunifront.dodeuni;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import dodeunifront.dodeuni.Hue.HueActivity;
+import dodeunifront.dodeuni.community.CommunityFragment;
+
+import androidx.appcompat.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
     final String TAG = this.getClass().getSimpleName();
@@ -22,9 +29,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init(); //객체 정의
         SettingListener(); //리스너 등록
-
+        Toolbar toolbar = findViewById (R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar (toolbar); //액티비티의 앱바(App Bar)로 지정
         bottomNavigationView.setSelectedItemId(R.id.menu_community);
+
+
     }
+
     private void init() {
         home = findViewById(R.id.content_layout);
         bottomNavigationView = findViewById(R.id.bottom_nav_view);
@@ -61,5 +73,27 @@ public class MainActivity extends AppCompatActivity {
 
             return false;
         }
+    }// 메뉴 리소스 XML의 내용을 앱바(App Bar)에 반영
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater ().inflate (R.menu.top_nav, menu);
+
+        return true;
     }
+
+    //앱바(App Bar)에 표시된 액션 또는 오버플로우 메뉴가 선택되면
+    //액티비티의 onOptionsItemSelected() 메서드가 호출
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId ()) {
+            case R.id.menu_hue:
+                startActivity (new Intent (this, HueActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected (item);
+        }
+    }
+
+
 }

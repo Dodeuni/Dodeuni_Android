0cfdf7fc0e94de428ee3d4af4a4486accdee3c95
yenidanan <yenidanan@sookmyung.ac.kr>
2023-05-16 17:28:02 +0900

    feat: 세부카테고리 3개메뉴 글전송, 댓글전송 구현(삭제,수정은 아직X)

---
+++ app/src/main/java/dodeunifront/dodeuni/community/API_Postcommunity.java
@@ -0,0 +1,59 @@
+package dodeunifront.dodeuni.community;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import dodeunifront.dodeuni.community.DTO.CommentResponseDTO;
+import dodeunifront.dodeuni.community.DTO.CommentSaveRequestDTO;
+import dodeunifront.dodeuni.community.DTO.CommunityDeatilDTO;
+import dodeunifront.dodeuni.community.DTO.CommunityListResponseDto;
+import dodeunifront.dodeuni.community.DTO.DTO_ResponseCommunity;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.PartMap;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface API_Postcommunity {
+    String URL = "http://192.168.0.18:8080/";
+
+    @Multipart
+    @POST("/api/community")
+    Call <DTO_ResponseCommunity> postData(
+            @Part("userId") Long userId,
+           // @Part("title") String title,
+           // @Part("content") String content,
+           // @Part("main") String main,
+            //@Part("sub") String sub,
+            @PartMap Map<String, RequestBody> map,
+            @Part ArrayList<MultipartBody.Part> photo
+    );
+
+    @POST("/api/comments")
+    Call <List<CommentResponseDTO>> postcomment(
+            @Body CommentSaveRequestDTO commentSaveRequestDTO
+    );
+    @Headers({"Accept: application/json"})
+    @GET("api/community/list")
+    Call<List<CommunityListResponseDto>> getDatalist(
+            @Query("main") String main,
+            @Query("sub") String sub
+    );
+
+    @GET("api/community/detail")
+    Call<CommunityDeatilDTO> getDatadetail(
+            @Query("id") Long id);
+
+    @GET("api/comments/list/{cid}")
+    Call <List<CommentResponseDTO>> getDataComment(
+            @Path("cid") Long cid
+    );
+}
--- app/src/main/java/dodeunifront/dodeuni/community/Smalltab_chinfo.java
+++ app/src/main/java/dodeunifront/dodeuni/community/Smalltab_chinfo.java
@@ -1,6 +1,7 @@
 package dodeunifront.dodeuni.community;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,15 +9,184 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import dodeunifront.dodeuni.R;
+import dodeunifront.dodeuni.community.Adapter.RegAdapter;
+import dodeunifront.dodeuni.community.DTO.CommunityListResponseDto;
+import dodeunifront.dodeuni.community.DTO.Data_PostCommunity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Smalltab_chinfo extends Fragment {
+    RecyclerView recyclerView;
+    RegAdapter regAdapter;
+    Data_PostCommunity regData;
+    private ArrayList<CommunityListResponseDto> regarrayList;
+    SwipeRefreshLayout swipeRefreshLayout;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_smalltab_chinfo, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_smalltab, container, false);
+
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.rv_community);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),RecyclerView.VERTICAL,false);
+
+        recyclerView.setLayoutManager(linearLayoutManager);
+        regarrayList = new ArrayList<>();
+        regAdapter = new RegAdapter(regarrayList);
+        recyclerView.setAdapter(regAdapter);
+        swipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.refresh_layout);
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API_Postcommunity.URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        API_Postcommunity api_postcommunity = retrofit.create(API_Postcommunity.class);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                api_postcommunity.getDatalist("정보","정보교환").enqueue(new Callback<List<CommunityListResponseDto>>() {
+                    @Override
+                    public void onResponse(Call<List<CommunityListResponseDto>> call, Response<List<CommunityListResponseDto>> response) {
+                        if (response.isSuccessful()) {
+                            if (response.body() != null) {
+                                ArrayList<CommunityListResponseDto> new_regarrayList = new ArrayList<>();;
+                                List<CommunityListResponseDto> datas = response.body();
+                                if (datas != null) {
+                                    for (int i = 0; i < datas.size(); i++) {
+                                        CommunityListResponseDto dict_0 = new CommunityListResponseDto(response.body().get(i).getId(),
+                                                response.body().get(i).getCreatedDateTime(),
+                                                response.body().get(i).getNickname(), response.body().get(i).getTitle(),
+                                                response.body().get(i).getContent(),response.body().get(i).getThumbnailUrl(),
+                                                response.body().get(i).getUserId());
+                                        Log.e("내용",response.body().get(i).getContent());
+                                        Log.e("제목",response.body().get(i).getTitle());
+                                        new_regarrayList.add(0,dict_0);
+                                    }
+                                    regarrayList.clear();
+                                    regarrayList.addAll(new_regarrayList);
+                                    regAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<List<CommunityListResponseDto>> call, Throwable t) {
+
+                    }
+                });
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+        api_postcommunity.getDatalist("정보","정보교환").enqueue(new Callback<List<CommunityListResponseDto>>() {
+            @Override
+            public void onResponse(Call<List<CommunityListResponseDto>> call, Response<List<CommunityListResponseDto>> response) {
+                if (response.isSuccessful()) {
+                    if (response.body() != null) {
+                        List<CommunityListResponseDto> datas = response.body();
+                        if (datas != null) {
+                            regarrayList.clear();
+                            for (int i = 0; i < datas.size(); i++) {
+                                CommunityListResponseDto dict_0 = new CommunityListResponseDto(response.body().get(i).getId(),
+                                        response.body().get(i).getCreatedDateTime(),
+                                        response.body().get(i).getNickname(), response.body().get(i).getTitle(),
+                                        response.body().get(i).getContent(),response.body().get(i).getThumbnailUrl(),
+                                        response.body().get(i).getUserId());
+                                Log.e("내용",response.body().get(i).getContent());
+                                Log.e("제목",response.body().get(i).getTitle());
+
+                                regarrayList.add(0,dict_0);
+                                regAdapter.notifyItemInserted(0);
+                            }
+                            Log.e("getdatalist end", "======================================");
+                        }
+                    }
+                }
+            }
+            @Override
+            public void onFailure(Call<List<CommunityListResponseDto>> call, Throwable t) {
+
+            }
+        });
+
+
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API_Postcommunity.URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        API_Postcommunity api_postcommunity = retrofit.create(API_Postcommunity.class);
+//        api_postcommunity.getDatadetail(Long.valueOf(1)).enqueue(new Callback<CommunityListResponseDto>() {
+//            @Override
+//            public void onResponse(Call<CommunityListResponseDto> call, Response<CommunityListResponseDto> response) {
+//                if (response.body() != null) {
+//                        CommunityListResponseDto datas = response.body();
+//                        if (datas != null) {
+//                            regarrayList.clear();
+//                                CommunityListResponseDto dict_0 = new CommunityListResponseDto(response.body().getCreatedDateTime(),
+//                                        response.body().getNickname(), response.body().getTitle(),
+//                                        response.body().getContent());
+//                                Log.e("내용",response.body().getContent());
+//                                Log.e("제목",response.body().getTitle());
+//
+//                                regarrayList.add(dict_0);
+//                                regAdapter.notifyItemInserted(0);
+//
+//                            Log.e("getdatalist end", "======================================");
+//                        }}
+//            }
+//
+//            @Override
+//            public void onFailure(Call<CommunityListResponseDto> call, Throwable t) {
+//
+//            }
+//        });
+
+
+       // Data_PostCommunity regdatas = new Data_PostCommunity(titel,content);
+        //regarrayList.add(regdatas);
+        //regAdapter.notifyItemInserted(0);
+
+       // Toast.makeText(getContext(), "불러오기 하였습니다..", Toast.LENGTH_SHORT).show();
+
+//        if (getArguments() != null){
+//            Bundle bundle = getArguments();
+//            String titles = bundle.getString("title");
+//            String contents = bundle.getString("content");
+//            RegData regdatas = new RegData(titles,contents);
+//            regarrayList.add(regdatas);
+//            regAdapter.notifyItemInserted(0);
+//        } else{
+//            Log.e("keyekyeky con",":lescontents");
+//        }
     }
 }
